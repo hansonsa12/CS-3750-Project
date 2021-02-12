58c417c06215c4b6a71504eef4ac384c821cfe6f
kkartchner <46807501+kkartchner@users.noreply.github.com>
2021-02-11 21:15:34 -0700

    Add users model

---
+++ Models/User.cs
@@ -0,0 +1,34 @@
+﻿namespace FinalProject.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public enum AccountType
+    {
+        Instructor,
+        Student,
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public DateTime BirthDay { get; set; }
+
+        [Required]
+        public AccountType AccountType { get; set; }
+    }
+}
