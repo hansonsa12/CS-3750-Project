ec579fb42bf991bbd86f72a67ca8df06a94ef1b6
hansonsa12 <samuelhanson@mail.weber.edu>
2021-02-24 20:02:38 -0700

    Added dashboard cards closes #16

--- ClientApp/src/components/AppContainer.js
+++ ClientApp/src/components/AppContainer.js
@@ -3,12 +3,32 @@ import axios from 'axios';
 import { authActions } from "../helpers/authActions";
 import SideNavigation from "./SideNavigation";
 import MainView from "./MainView";
-import { withStyles, CssBaseline } from "@material-ui/core";
+import { withStyles, CssBaseline, CardContent, Typography } from "@material-ui/core";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+
 
 const styles = theme => ({
     root: {
         display: 'flex'
+    },
+});
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
     },
 });
 
@@ -32,6 +52,73 @@ class AppContainer extends Component {
                     </Route>
                     <Route exact path="/dashboard">
                         <MainView title={`Dashboard`}>
+                            <Card>
+                                <CardContent>
+                                    <Typography className={classes.title} variant="h5" color="textPrimary" gutterBottom>
+                                        CS3550 Advanced Database Programming
+                                    </Typography>
+                                    <Typography>
+                                        Instructor Name
+                                    </Typography>
+                                    <Typography>
+                                        CA 141
+                                    </Typography>
+                                    <Typography>
+                                        Meeting Time
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                            <br></br>
+                            <Card className={classes.root}>
+                                <CardContent>
+                                    <Typography className={classes.title} variant="h5" color="textPrimary" gutterBottom>
+                                        CS3750 Software Engineering II
+                                    </Typography>
+                                    <Typography>
+                                        Instructor Name
+                                    </Typography>
+                                    <Typography>
+                                        CA 155
+                                    </Typography>
+                                    <Typography>
+                                        Meeting Time
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                            <br></br>
+                            <Card className={classes.root}>
+                                <CardContent>
+                                    <Typography className={classes.title} variant="h5" color="textPrimary" gutterBottom>
+                                        CS3540 Database Administration
+                                    </Typography>
+                                    <Typography>
+                                        Instructor Name
+                                    </Typography>
+                                    <Typography>
+                                        CA 175
+                                    </Typography>
+                                    <Typography>
+                                        Meeting Time
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                            <br></br>
+                            <Card className={classes.root}>
+                                <CardContent>
+                                    <Typography className={classes.title} variant="h5" color="textPrimary" gutterBottom>
+                                        CS3230 Object Oriented User Interface Development with Java
+                                    </Typography>
+                                    <Typography>
+                                        Instructor Name
+                                    </Typography>
+                                    <Typography>
+                                        CA 240
+                                    </Typography>
+                                    <Typography>
+                                        Meeting Time
+                                    </Typography>
+                                </CardContent>
+                            </Card>
                         </MainView>
                     </Route>
                     <Route exact path="/calendar">
