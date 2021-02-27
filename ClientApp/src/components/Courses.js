e14e31fc0f69fd774ebc851213350edbcc543048
kkartchner <46807501+kkartchner@users.noreply.github.com>
2021-02-27 12:58:17 -0700

    (1) Side Nav updates Fixes #34

---
+++ ClientApp/src/components/Courses.js
@@ -0,0 +1,8 @@
+import { Button } from '@material-ui/core'
+import React from 'react'
+
+export default function Courses() {
+    return (
+        <Button color="primary">Add A Course</Button>
+    )
+}
