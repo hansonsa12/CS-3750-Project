091dfb63a66da6fc61b2b76d7f161e8c537e21bf
hansonsa12 <samuelhanson@mail.weber.edu>
2021-03-19 12:04:18 -0600

    Assignment model Fixes #65

---
+++ Models/Course/Assignment.cs
@@ -0,0 +1,35 @@
+namespace final_project.Models.Course
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class AssignmentInfo {
+
+        [Key]
+        public int AssignmentId { get; set; }
+
+
+        [Required]
+        public int CourseId { get; set; }
+
+        [Required]
+        [Column (TypeName = "varchar(60)")]
+        public string Title { get; set; }
+
+        [Column (TypeName = "text")]
+        public string Description { get; set; }
+
+        public int? MaxPoints { get; set; }
+
+        [Column (TypeName = "DATETIME")]
+        public DateTime? DueDate { get; set; }
+
+        [Required]
+        [Column (TypeName = "varchar(10)")]
+        public string AssignmentType { get; set; }
+
+
+
+    }
+}
