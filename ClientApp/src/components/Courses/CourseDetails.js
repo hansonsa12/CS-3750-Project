32cdcbdbb712b0e436c41ff0aa4bca222ca5a0ec
kkartchner <46807501+kkartchner@users.noreply.github.com>
2021-03-25 00:27:51 -0600

    Course Details page: - page now has course info that it gets using the course id.

--- ClientApp/src/components/Courses/CourseDetails.js
+++ ClientApp/src/components/Courses/CourseDetails.js
@@ -1,6 +1,9 @@
-import React, { useContext } from 'react';
+import { Grid } from '@material-ui/core';
+import _ from 'lodash';
+import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { AuthContext } from '../../context/AuthProvider';
+import { DataContext } from '../../context/DataProvider';
 import AssignmentForm from '../Assignments/AssignmentForm';
 import MainView from '../MainView';
 import TableComponent from '../TableComponent';
@@ -9,13 +12,23 @@ export default function CourseDetails() {
     const { isInstructor } = useContext(AuthContext);
     const { id } = useParams(); // get params from route url
 
+    const { courses } = useContext(DataContext);
+
+    const [course, setCourse] = useState({});
+
+    useEffect(() => {
+        setCourse(courses.find((c) => c.courseId.toString() === id))
+    }, [courses])
+
+
+
     const actionProps = isInstructor ? {
         action: <AssignmentForm courseId={id} />,
         actionTooltip: "Add New Assignment"
     } : {};
 
     return (
-        <MainView title={`Course ${id}`} {...actionProps}>
+        <MainView title={course?.courseName} {...actionProps}>
             {isInstructor && (
                 <>
                     <TableComponent course />
