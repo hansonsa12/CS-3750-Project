377a13277c56f4a136e60f7e6e5fc73d27269912
Der3k <42587743+CountDer3k@users.noreply.github.com>
2021-04-08 16:13:44 -0600

    Show class meeting times on calendar Fixes #93

--- ClientApp/src/components/Calendar.js
+++ ClientApp/src/components/Calendar.js
@@ -1,18 +1,44 @@
 ﻿import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid'; // must come after FullCalendar import
-import React from 'react';
+import React, { useContext } from 'react';
+import { DataContext } from '../context/DataProvider';
 
-export default class Calendar extends React.Component {
-    render() {
-        return (
-            <FullCalendar
-                plugins={[dayGridPlugin]}
-                initialView="dayGridMonth"
-                events={[
-                    { title: 'Pre-Team Meeting', date: '2021-02-24' },
-                    { title: 'Team Meeting', date: '2021-02-25' }
-                ]}
-            />
-        )
+
+function getCalEvents(course, ucLen)
+{
+    const dayNums = {m: 1, t:2, w:3, r:4, f:5};
+    const daysOfWeek = course.meetingDays.toLowerCase().split('').map(d => dayNums[d]);
+    console.log('Days: '+ daysOfWeek)
+    var callEvents = [];
+    for(var i = 0; i < ucLen; i++)
+    {
+        console.log("in loop")
+        callEvents.push({
+            title: course.courseName,
+            startTime: course.startTime,
+            endTime: course.endTime,
+            daysOfWeek: daysOfWeek
+        })
     }
+    return callEvents;
 }
+
+export default function Calendar(){
+    const { userCourses } = useContext(DataContext);
+
+    var callEvents=[];
+    userCourses.map(course => (
+        callEvents = getCalEvents(course, userCourses.length)
+    ))
+    console.log(callEvents)
+
+    return (
+        
+        <FullCalendar
+            plugins={[dayGridPlugin]}
+            initialView='dayGridMonth'
+            events={callEvents}
+        />
+    )
+}
+
