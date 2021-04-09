5580497b9cf89556fa60b414450245219ca266ba
Der3k <42587743+CountDer3k@users.noreply.github.com>
2021-04-08 22:44:34 -0600

    Show class meeting times on calendar (#102)
    - Add userCourses to the calendar based on meeting days and start/end times

--- ClientApp/src/components/Calendar.js
+++ ClientApp/src/components/Calendar.js
@@ -1,18 +1,45 @@
 ﻿import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"; // must come after FullCalendar import
-import React from "react";
+import React, { useContext, useMemo } from "react";
+import { useHistory } from "react-router-dom";
+import { DataContext } from "../context/DataProvider";
 
-export default class Calendar extends React.Component {
-    render() {
-        return (
-            <FullCalendar
-                plugins={[dayGridPlugin]}
-                initialView="dayGridMonth"
-                events={[
-                    { title: "Pre-Team Meeting", date: "2021-02-24" },
-                    { title: "Team Meeting", date: "2021-02-25" }
-                ]}
-            />
-        );
-    }
+export default function Calendar() {
+    const { userCourses } = useContext(DataContext);
+
+    const history = useHistory(); // https://reactrouter.com/web/api/Hooks
+
+    const dayNums = { m: 1, t: 2, w: 3, r: 4, f: 5 };
+
+    const events = useMemo(
+        /* Optimization technique. Memoizes (caches) the array of events so that when this compoent 
+        rerenders, the map function is only called again if the userCourses changes. Otherwise it
+        just returns the cached array. https://reactjs.org/docs/hooks-reference.html#usememo */
+        () =>
+            userCourses.map(course => ({
+                title: `${course.courseNumber} ${course.courseName}`,
+                startTime: course.startTime,
+                endTime: course.endTime,
+                daysOfWeek: course.meetingDays
+                    ?.toLowerCase()
+                    .split("")
+                    .map(d => dayNums[d]),
+                url: `/courses/${course.courseId}/details`
+            })),
+        [userCourses]
+    );
+
+    return (
+        <FullCalendar
+            plugins={[dayGridPlugin]}
+            initialView="dayGridMonth"
+            events={events}
+            eventClick={info => {
+                /* link event to its corresponding course page 
+                https://fullcalendar.io/docs/eventClick */
+                info.jsEvent.preventDefault();
+                history.push(info.event.url);
+            }}
+        />
+    );
 }
