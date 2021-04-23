3f217dad0aa1b40d18dc73db2e60153fa590b648
kkartchner <46807501+kkartchner@users.noreply.github.com>
2021-04-23 11:46:18 -0600

    Add test project

---
+++ final_project.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+
+namespace final_project.Tests
+{
+    using System;
+    using Xunit;
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true, "should be true");
+        }
+    }
+}
