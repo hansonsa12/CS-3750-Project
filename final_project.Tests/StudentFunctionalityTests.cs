168f3d55e9d433f1e7c029936a9bac11aa42b02d
kkartchner <46807501+kkartchner@users.noreply.github.com>
2021-04-24 10:26:08 -0600

    Test 3: Student can drop a class
     Closes #124 (#140)

--- final_project.Tests/StudentFunctionalityTests.cs
+++ final_project.Tests/StudentFunctionalityTests.cs
@@ -1,13 +1,8 @@
 namespace final_project.Tests
 {
-    using System.IdentityModel.Tokens.Jwt;
-    using System.Security.Claims;
     using final_project.Controllers;
     using final_project.Models.Course;
-    using final_project.Models.User;
     using final_project.Tests.Shared;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Mvc;
     using Xunit;
 
     public class StudentFunctionalityTests : IClassFixture<SharedDatabaseFixture>
@@ -51,6 +46,47 @@ namespace final_project.Tests
                 // should now have one registered course
                 Assert.True(student.Registrations.Count == initialRegistrationsCount + 1,
                     "Student registrations increased by 1");
+
+                #endregion
+            }
+        }
+
+        [Fact]
+        public async void StudentCanDropACourse()
+        {
+            using (var context = Fixture.CreateContext())
+            {
+                // Steps for test: (1) Setup, (2) Exercise, (3) Verify
+                // region tags just used for organization. not needed
+                #region Setup
+                var controller = new RegistrationsController(context);
+                TestHelper testHelper = new TestHelper(context, controller);
+                // Student doesn't matter here so I am just using a default
+                var student = testHelper.GetDefaultStudent();
+
+                // Mocks the user being logged in. Must be a user added in the database.
+                testHelper.Login(student);
+
+                Course newCourse = testHelper.CreateACourse();
+
+                student.Registrations.Add(newCourse);
+                context.SaveChanges();
+
+                // Get initial registrations count
+                var initialRegistrationsCount = student.Registrations.Count;
+
+                #endregion
+
+                #region Exercise
+                // create and register for a new course
+                var result = await controller.DropCourse(newCourse.CourseId);
+
+                #endregion
+
+                #region Verify
+                // should now have one registered course
+                Assert.True(student.Registrations.Count == initialRegistrationsCount - 1,
+                    "Student registrations decreased by 1");
 
                 #endregion
             }
